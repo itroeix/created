4c611c0044d8512fd0c942eb199849058c933aad
itroeix <91840330+itroeix@users.noreply.github.com>
2022-03-31 21:24:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Nothing here...")
+
+function update() {
+    const dateNow = Math.round(Date.now() / 1000)
+    const actual = dateNow - 1648747239;
+
+
+    console.log("🎉 Updated 🎉")
+    const resultado = document.querySelector('.update');
+    resultado.textContent = `This website was created ${actual} seconds ago.`;
+}
+
+
+// -AutoUpdate-
+//setInterval(update, 1000);
